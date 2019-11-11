1241dd5c44bbfdd280cfa7ff24b8d4a77a06587a
toto <ro.marcus@free.fr>
2019-11-11 15:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function add( a, b ) {
   return ( a + b );
 }
+function add( a, b ) {
+  return ( a - b );
+}
+function add( a, b ) {
+  return ( a * b );
+}
+function add( a, b ) {
+  return ( a / b );
+}
