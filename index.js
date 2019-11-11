9e9505c17b9be8718c054e0ef229ce4ffdaa07f1
toto <ro.marcus@free.fr>
2019-11-11 16:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function dec( n ) {
   return ( n = n - 1 );
 }
 function makeInt(n) {
-  parseInt( n, 10 );
+  parseInt(n, 10);
   return n;
 }
 function preserveDecimal(n) {
-  parseFloat( n );
+  parseFloat(n);
   return n;
 }
